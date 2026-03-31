a91a652c772a67017876f5db8ba1ffb0edf42b94
mosii721 <mikealwanga97@gmail.com>
2026-04-01 02:30:17 +0300

    Made ping Public

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Head } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @ApiTags('app')
 @Controller()
 export class AppController {
+
+    @Public()
     @ApiQuery({ name: 'ping', required: false , description: 'Simple endpoint to check if the backend is running' })
     @Get('ping')
     @Head('ping')
